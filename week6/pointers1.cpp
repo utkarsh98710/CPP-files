49c638bfff8d026d95d152939437097f655ed8f0
utkarsh89573 <utkarshgupta89573@gmail.com>
2024-03-01 12:24:57 +0530

    new commit

---
+++ week6/pointers1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int b=5;
+    cout<<a<<endl;
+    
+    //Addres of a and b
+    cout<<&a<<endl;
+    cout<<&b<<endl;
+
+
+}
