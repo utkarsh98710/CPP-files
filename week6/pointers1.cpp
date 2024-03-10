88f765ffa53fb4cbc69c215e2235ab39228d713b
utkarsh89573 <utkarshgupta89573@gmail.com>
2024-03-10 23:57:00 +0530

    commit

--- week6/pointers1.cpp
+++ week6/pointers1.cpp
@@ -1,13 +1,28 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a=5;
-    int b=5;
-    cout<<a<<endl;
+    // int a=5;
+    // int b=5;
+    // cout<<a<<endl;
     
-    //Addres of a and b
-    cout<<&a<<endl;
-    cout<<&b<<endl;
+    // //Addres of a and b
+    // cout<<&a<<endl;
+    // cout<<&b<<endl;
 
+
+    int a=5;
+    //pointer create
+    int*ptr = &a; //ptr is a pointer to A which contain a (ptr a ke address ko point kar raha he)
+
+    //access the value ptr is pointing to 
+    // de-reference of ptr..
+    cout<< "Address of a is:"<< &a<<endl; 
+    cout<<"value stores at ptr is: "<<ptr<<endl;
+    cout<< "value ptr is pointing to is: "<<*ptr<<endl;
+    cout<<"address of ptr"<<&ptr<<endl;
+
+
+
+ 
 
 }
