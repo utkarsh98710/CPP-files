ac0d4fc7a4d68e76cc3bb1b38ca86c6a5aef0360
utkarsh98710 <156845520+utkarsh98710@users.noreply.github.com>
2024-07-21 20:32:18 +0530

    oops continue

---
+++ oops2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    class Animal{
+        private:
+        int age;
+        int weight;
+
+    public:
+
+        void eat(){
+            cout<<"I am eating"<<endl;
+        }
+
+        int getAge(){
+            return this->Age;
+        }
+        void setAge()
+    }
+}
+
